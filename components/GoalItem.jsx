a2bcc3da4ac79f79285bb27628e5e3fb1498569e
ritesh2320 <riteshshelar820@gmail.com>
2025-01-25 13:37:34 +0530

    remove extra lines

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -4,14 +4,12 @@ import {StyleSheet, View, Text, Pressable} from 'react-native';
 function GoalItem(props) {
   return (
     <>
-      <View style={styles.goalList}>
-        <View style={styles.goalsItem}>
-          <Pressable
-            android_ripple={{color: '#8e44ad'}}
-            onPress={props.deleteGoal.bind(this, props.id)}>
-            <Text style={styles.goalsText}>{props.text}</Text>
-          </Pressable>
-        </View>
+      <View style={styles.goalsItem}>
+        <Pressable
+          android_ripple={{color: '#8e44ad'}}
+          onPress={props.deleteGoal.bind(this, props.id)}>
+          <Text style={styles.goalsText}>{props.text}</Text>
+        </Pressable>
       </View>
     </>
   );
@@ -20,9 +18,6 @@ function GoalItem(props) {
 // for ios apps to get an riple action. to achieve this in pressable tag add below line-->style={pressed => pressed && styles.pressedItems}
 
 const styles = StyleSheet.create({
-  goalList: {
-    // marginTop: 20,
-  },
   goalsItem: {
     margin: 8,
     // backgroundColor: '#5b2c6f',
